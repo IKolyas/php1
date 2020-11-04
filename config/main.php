5f955e150ba69ad1325d830e665fcad1f622e68a
IKolyas <i.kolyasnikov@mail.ru>
2020-11-04 21:41:17 +0500

    Update main.php

--- config/main.php
+++ config/main.php
@@ -5,7 +5,6 @@ define('ENGINE_DIR', ROOT_DIR . 'engine/');
 define('IMG_DIR', PUBLIC_DIR . 'img/');
 define('VIEWS_DIR', ROOT_DIR . 'views/');
 define('CONFIG_DIR', ROOT_DIR . 'config/');
-define('VENDOR', ROOT_DIR . 'vendor/');
 define('PAGES_DIR', ROOT_DIR . 'pages/');
 define('DEFAULT_CONTROLLER', 'product');
 define('DEFAULT_ACTION', 'index');
